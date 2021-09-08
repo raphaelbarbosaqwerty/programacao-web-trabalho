976bffa498cd994f5db8f2e6883360d4cf085b7b
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-09-08 19:40:41 -0300

    Finishing configuration

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,8 @@
-function goToContact(){
+// TODO - Vamos usar aqui para fazer as requisições
+function goToContact() {
     location.href = "contact.html";
 }
 
 function routeToPost(id) {
-    location.href = "post/"+id;
+    location.href = "post/" + id;
 }
